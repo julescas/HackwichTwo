4452e5cf7f07834a9c70eff9097d66fd45aba919
julescas <112312841+julescas@users.noreply.github.com>
2022-09-14 20:03:53 -1000

    created another button part 8 hackwich two assignment

--- HackwichTwo/ViewController.swift
+++ HackwichTwo/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //this is the code block func for ViewDidLoad
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    @IBAction func pressMeAgainButtonPressed(_ sender: Any) {
+        
+     print("I got this!")
+        
+        
+        
+    }
+    
+    
+    @IBAction func pressMeButtonPressed(_ sender: Any) {
+        
+    print("Hello World")
+        
+    }
+    
+    
+    
 }
 
